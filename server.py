392920c3f7b81182ed7772893c784669626ec7d1
Nitish Kumar Yarlagadda <nitishkumar8.y@gmail.com>
2025-09-29 21:05:23 -0500

    Final webpage

--- server.py
+++ server.py
@@ -1,39 +1,57 @@
-from flask import Flask, render_template, request, redirect
-import psycopg2
+from flask import Flask, render_template, request, redirect, url_for
+from contextlib import contextmanager
 import os
-from dotenv import load_dotenv
+import psycopg2
+from psycopg2.pool import ThreadedConnectionPool
+from psycopg2.extras import DictCursor
 
-load_dotenv()
+pool = None
 
+def setup_db_pool():
+    global pool
+    DATABASE_URL = os.environ['DATABASE_URL']
+    print(f"Creating DB connection pool for {DATABASE_URL}")
+    pool = ThreadedConnectionPool(1, 20, dsn=DATABASE_URL, sslmode='require')
+
+@contextmanager
+def get_db_connection():
+    connection = pool.getconn()
+    try:
+        yield connection
+    finally:
+        pool.putconn(connection)
+
+@contextmanager
+def get_db_cursor(commit=False):
+    with get_db_connection() as conn:
+        cur = conn.cursor(cursor_factory=DictCursor)
+        try:
+            yield cur
+            if commit:
+                conn.commit()
+        finally:
+            cur.close()
 app = Flask(__name__)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-conn = psycopg2.connect(DATABASE_URL)
+setup_db_pool()
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    name = request.args.get('name')
-
-    if request.method == 'POST':
-        form_name = request.form.get('name')
-        message = request.form.get('message')
-
-        if form_name and message:
-            with conn.cursor() as cur:
+@app.route('/', methods=["GET", "POST"])
+def guest_list():
+    if request.method == "POST":
+        name = request.form.get("name")
+        message = request.form.get("message")
+        if name and message:
+            with get_db_cursor(commit=True) as cur:
                 cur.execute(
-                    "INSERT INTO guest_list (name, message) VALUES (%s, %s)",
-                    (form_name, message)
+                    "INSERT INTO guest_list (name, message) VALUES (%s, %s);",
+                    (name, message)
                 )
-                conn.commit()
-        return redirect('/')
+        return redirect(url_for("guest_list"))
 
-    with conn.cursor() as cur:
-        cur.execute("SELECT name, message FROM guest_list ORDER BY id ASC")
-        rows = cur.fetchall()
+    with get_db_cursor() as cur:
+        cur.execute("SELECT name, message FROM guest_list ORDER BY id DESC;")
+        guests = cur.fetchall()
 
-    guests = [{'name': r[0], 'message': r[1]} for r in rows]
+    return render_template("hello.html", guest_list=guests)
 
-    return render_template('hello.html', guests=guests, name=name)
 
-if __name__ == '__main__':
-    app.run(debug=True)
